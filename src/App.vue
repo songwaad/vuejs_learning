0d83c2925d0ec3f4a89836000b0ad6eb9fa7207c
Songwaad <Songw44d@gmail.com>
2025-04-05 11:36:33 +0700

    binding event : inline handler

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <script setup>
 import { ref } from 'vue';
 
-const currentLink = ref('https://google.com')
-
-const changeLink = () => {
-  currentLink.value = 'https://youtube.com'
-};
+const count = ref(0);
 </script>
 
 <template>
   <div>
-    <a href="currentLink" target="_blank">Click to go to {{ currentLink }}</a>
-    <button @click="changeLink()">Click to change</button>
+    <button @click="count++">Add 1</button>
+    <p>Count is: {{ count }}</p>
   </div>
 </template>
