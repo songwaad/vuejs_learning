42fd5c0d9b2086c84116ffab4ed6e4b39b62eeeb
Songwaad <Songw44d@gmail.com>
2025-04-05 11:34:24 +0700

    attribute bindings

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup>
 import { ref } from 'vue';
 
-const searchText = ref('');
+const currentLink = ref('https://google.com')
+
+const changeLink = () => {
+  currentLink.value = 'https://youtube.com'
+};
 </script>
 
 <template>
-  <div>{{ searchText }}</div>
-  <input v-model="searchText">
+  <div>
+    <a href="currentLink" target="_blank">Click to go to {{ currentLink }}</a>
+    <button @click="changeLink()">Click to change</button>
+  </div>
 </template>
